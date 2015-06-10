2ab319f6d39e00d4ed428a0cbd9bf039752580bd
Bryan Keiren <bryankeiren@gmail.com>
2015-06-10 21:56:33 +0200

    Added contributors entry.

--- EasyImgur/Contributors.cs
+++ EasyImgur/Contributors.cs
@@ -16,13 +16,20 @@ namespace EasyImgur
 
             public override string ToString()
             {
-                return name + " (" + alias + ")";
+                String str = name;
+                if (!String.IsNullOrEmpty(alias))
+                    str += " (" + alias + ")";
+                return str;
             }
         }
 
         static public List<Contributor> contributors = new List<Contributor> {
             new Contributor { name = "Alex van Liew",           alias = "snoozbuster",              url = "https://github.com/snoozbuster"      },
-            new Contributor { name = "Joona Heikkilä",          alias = "cubrr",                    url = "https://github.com/cubrr"            }
+            new Contributor { name = "Joona Heikkilä",          alias = "cubrr",                    url = "https://github.com/cubrr"            },
+
+
+            // Leave this for last at all times.
+            new Contributor { name = "and every user of EasyImgur",  alias = "",                    url = ""                                    }
         };
 
         static public BindingSource bindingSource = new BindingSource();
