b530281b92d7de3748bd52a666aa69e30540e871
cubrr <jay@thecuber.org>
2015-06-12 01:53:36 +0300

    Refactor AuthorizeForm

--- EasyImgur/AuthorizeForm.Designer.cs
+++ EasyImgur/AuthorizeForm.Designer.cs
@@ -38,36 +38,42 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(49, 19);
+            this.label1.Location = new System.Drawing.Point(29, 23);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(378, 17);
+            this.label1.Size = new System.Drawing.Size(268, 13);
             this.label1.TabIndex = 6;
-            this.label1.Text = "Enter the PIN number you\'ve received from the Imgur page";
+            this.label1.Text = "Please enter the PIN from the Imgur authorization page.";
             // 
             // textBoxPIN
             // 
-            this.textBoxPIN.Location = new System.Drawing.Point(100, 53);
+            this.textBoxPIN.Location = new System.Drawing.Point(32, 52);
+            this.textBoxPIN.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.textBoxPIN.Name = "textBoxPIN";
-            this.textBoxPIN.Size = new System.Drawing.Size(244, 22);
+            this.textBoxPIN.Size = new System.Drawing.Size(265, 20);
             this.textBoxPIN.TabIndex = 5;
             this.textBoxPIN.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
             // buttonOK
             // 
-            this.buttonOK.Location = new System.Drawing.Point(114, 96);
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(165, 89);
+            this.buttonOK.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonOK.Name = "buttonOK";
-            this.buttonOK.Size = new System.Drawing.Size(97, 27);
+            this.buttonOK.Size = new System.Drawing.Size(73, 22);
             this.buttonOK.TabIndex = 9;
-            this.buttonOK.Text = "OK";
+            this.buttonOK.Text = "Authorize";
             this.buttonOK.UseVisualStyleBackColor = true;
             this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
             // 
             // buttonCancel
             // 
+            this.buttonCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
             this.buttonCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.buttonCancel.Location = new System.Drawing.Point(229, 96);
+            this.buttonCancel.Location = new System.Drawing.Point(246, 89);
+            this.buttonCancel.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.buttonCancel.Name = "buttonCancel";
-            this.buttonCancel.Size = new System.Drawing.Size(97, 27);
+            this.buttonCancel.Size = new System.Drawing.Size(73, 22);
             this.buttonCancel.TabIndex = 10;
             this.buttonCancel.Text = "Cancel";
             this.buttonCancel.UseVisualStyleBackColor = true;
@@ -76,15 +82,16 @@
             // AuthorizeForm
             // 
             this.AcceptButton = this.buttonOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.CancelButton = this.buttonCancel;
-            this.ClientSize = new System.Drawing.Size(439, 135);
+            this.ClientSize = new System.Drawing.Size(330, 122);
             this.Controls.Add(this.buttonCancel);
             this.Controls.Add(this.buttonOK);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBoxPIN);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "AuthorizeForm";
             this.Text = "Authorization PIN";
             this.Load += new System.EventHandler(this.AccountCredentialsForm_Load);
