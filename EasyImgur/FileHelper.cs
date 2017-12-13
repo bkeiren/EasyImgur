cc9e6a0a0e5a6372a5494abc78ff9fffe1506059
Bryan Keiren <bkeiren@users.noreply.github.com>
2017-12-13 01:05:46 +0100

    Album creation and adding of images to album are now separated in ImgurAPI
    Uploading images to an album now happens in separate calls to the ImgurAPI class. This lets us load the image one by one instead of loading them all into memory at once before starting the upload.

--- EasyImgur/FileHelper.cs
+++ EasyImgur/FileHelper.cs
@@ -19,12 +19,12 @@ namespace EasyImgur
         /// </summary>
         /// <param name="location">Path of the file to write</param>
         /// <param name="contents">Text to write to file</param>
-        public static void GZipWriteFile(string location, string contents)
+        public static void GZipWriteFile( string _Location, string _Contents )
         {
-            byte[] sourceBytes = Encoding.UTF8.GetBytes(contents); // UTF-8 without BOM
+            byte[] sourceBytes = Encoding.UTF8.GetBytes(_Contents); // UTF-8 without BOM
 
             using (var inStream = new MemoryStream(sourceBytes))
-            using (var fs = new FileStream(location, FileMode.Create, FileAccess.Write)) // Open file for writing, overwrite existing
+            using (var fs = new FileStream(_Location, FileMode.Create, FileAccess.Write)) // Open file for writing, overwrite existing
             using (var outStream = new GZipStream(fs, CompressionMode.Compress))
             {
                 inStream.CopyTo(outStream);
@@ -36,16 +36,16 @@ namespace EasyImgur
         /// </summary>
         /// <param name="location">Path of the file to read.</param>
         /// <returns>Decompressed contents of the file.</returns>
-        public static string GZipReadFile(string location)
+        public static string GZipReadFile( string _Location )
         {
             // 4096 is the default buffer size. Don't know if PeekBytes conflicts with
             // SequentialScan but there doesn't appear to be any difference.
-            using (var fs = new FileStream(location, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan))
+            using (var fs = new FileStream(_Location, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan))
             {
                 Stream inStream;
 
                 // Check if file starts with the gzip magic number
-                if (BitConverter.ToUInt16(fs.PeekBytes(2), 0) != GZipMagicNumber) // Could this be shorter with `?:`? Yes! Would it be legible? No!
+                if (BitConverter.ToUInt16(PeekBytes(fs, 2), 0) != GZipMagicNumber) // Could this be shorter with `?:`? Yes! Would it be legible? No!
                 {
                     // Not gzip-compressed, probably old history file version in plain text
                     // or machine is big-endian. We'll worry about the latter when someone
@@ -73,60 +73,15 @@ namespace EasyImgur
         /// <param name="stream">Stream to peek</param>
         /// <param name="count">Amount of bytes to peek</param>
         /// <returns>Peeked bytes</returns>
-        public static byte[] PeekBytes(this Stream stream, int count)
+        public static byte[] PeekBytes( Stream _Stream, int _Count )
         {
-            var buffer = new byte[count];
-            if (!stream.CanSeek)
+            var buffer = new byte[_Count];
+            if (!_Stream.CanSeek)
                 return buffer;
 
-            stream.Read(buffer, 0, count);
-            stream.Seek(0, SeekOrigin.Begin);
+            _Stream.Read(buffer, 0, _Count);
+            _Stream.Seek(0, SeekOrigin.Begin);
             return buffer;
-        }
-
-        public static bool CheckIfValidImageFileHeader(string path)
-        {
-            using (var fs = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.Read, 4096, FileOptions.SequentialScan))
-            {
-                var bytes = fs.PeekBytes(4);
-                var imgFormatFromHeader = GetImageFormat(bytes);
-                return imgFormatFromHeader != null;
-            }
-        }
-
-        public static System.Drawing.Imaging.ImageFormat GetImageFormat(byte[] bytes)
-        {
-            // see http://www.mikekunz.com/image_file_header.html  
-            var bmp = Encoding.ASCII.GetBytes("BM");     // BMP
-            var gif = Encoding.ASCII.GetBytes("GIF");    // GIF
-            var png = new byte[] { 137, 80, 78, 71 };    // PNG
-            var tiff = new byte[] { 73, 73, 42 };         // TIFF
-            var tiff2 = new byte[] { 77, 77, 42 };         // TIFF
-            var jpeg = new byte[] { 255, 216, 255, 224 }; // jpeg
-            var jpeg2 = new byte[] { 255, 216, 255, 225 }; // jpeg canon
-
-            if (bmp.SequenceEqual(bytes.Take(bmp.Length)))
-                return ImageFormat.Bmp;
-
-            if (gif.SequenceEqual(bytes.Take(gif.Length)))
-                return ImageFormat.Gif;
-
-            if (png.SequenceEqual(bytes.Take(png.Length)))
-                return ImageFormat.Png;
-
-            if (tiff.SequenceEqual(bytes.Take(tiff.Length)))
-                return ImageFormat.Tiff;
-
-            if (tiff2.SequenceEqual(bytes.Take(tiff2.Length)))
-                return ImageFormat.Tiff;
-
-            if (jpeg.SequenceEqual(bytes.Take(jpeg.Length)))
-                return ImageFormat.Jpeg;
-
-            if (jpeg2.SequenceEqual(bytes.Take(jpeg2.Length)))
-                return ImageFormat.Jpeg;
-
-            return null;
         }
     }
 }
